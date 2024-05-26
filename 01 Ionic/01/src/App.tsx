b85456513109de503eadd8d1a82947df65b6f315
Michal Radomski <michalra@op.pl>
2024-05-26 22:34:19 +0200

    Ionic-React course - commit 05

--- 01 Ionic/01/src/App.tsx
+++ 01 Ionic/01/src/App.tsx
@@ -1,8 +1,33 @@
 import React from "react";
-import { IonButton, IonIcon, setupIonicReact, useIonToast } from "@ionic/react";
+import {
+  IonApp,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+  setupIonicReact,
+  useIonToast,
+} from "@ionic/react";
 import { play as playIcon } from "ionicons/icons";
 
+// Core CSS required for Ionic components to work properly
 import "@ionic/react/css/core.css";
+
+// Basic CSS for apps built with Ionic
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+// Optional CSS utils that can be commented out
+import "@ionic/react/css/padding.css";
+// import '@ionic/react/css/float-elements.css';
+// import '@ionic/react/css/text-alignment.css';
+// import '@ionic/react/css/text-transformation.css';
+// import '@ionic/react/css/flex-utils.css';
+// import '@ionic/react/css/display.css';
+
 import "./App.scss";
 
 setupIonicReact();
@@ -23,29 +48,39 @@ const App = (): JSX.Element => {
 
   return (
     <React.Fragment>
-      <header>
-        <h1>Ionic-React App</h1>
-      </header>
-
-      <main>
-        <IonButton
-          color={"tertiary"}
-          shape="round"
+      <IonApp>
+        <IonHeader placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+          <IonToolbar placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+            <IonTitle placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+              Ionic-React App
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent
+          className="ion-padding"
           placeholder={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}
-          onClick={handleClick}
         >
-          <IonIcon
-            icon={playIcon}
-            slot="start"
+          <IonButton
+            color={"tertiary"}
+            shape="round"
             placeholder={undefined}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
-          />{" "}
-          Click Me
-        </IonButton>
-      </main>
+            onClick={handleClick}
+          >
+            <IonIcon
+              icon={playIcon}
+              slot="start"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            />{" "}
+            Click Me
+          </IonButton>
+        </IonContent>
+      </IonApp>
     </React.Fragment>
   );
 };
