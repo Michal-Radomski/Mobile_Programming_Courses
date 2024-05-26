4ae4ba859e014a782d28a8f9790bcb1176398904
Michal Radomski <michalra@op.pl>
2024-05-26 17:20:36 +0200

    Ionic-React course - commit 01

--- 01 Ionic/01/src/App.tsx
+++ 01 Ionic/01/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { IonButton, setupIonicReact } from "@ionic/react";
 
+import "@ionic/react/css/core.css";
 import "./App.scss";
 
+setupIonicReact();
+
 const App = (): JSX.Element => {
-  return <React.Fragment>Ionic React App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <header>
+        <h1>Ionic-React App</h1>
+      </header>
+
+      <main>
+        <IonButton placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+          Click Me
+        </IonButton>
+      </main>
+    </React.Fragment>
+  );
 };
 
 export default App;
