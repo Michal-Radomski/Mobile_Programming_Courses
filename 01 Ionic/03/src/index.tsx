a17bf375311ba8fa035b8a88ddaff595dc4911b5
Michal Radomski <michalra@op.pl>
2024-05-31 01:45:17 +0200

    Ionic-React course - commit 22

--- 01 Ionic/03/src/index.tsx
+++ 01 Ionic/03/src/index.tsx
@@ -3,6 +3,28 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import { SplashScreen } from "@capacitor/splash-screen";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Theme variables */
+import "./theme/variables.scss";
+
+// Hide the splash (you should do this on app launch)
 
 const container = document.getElementById("root");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -12,6 +34,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+SplashScreen.hide();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
