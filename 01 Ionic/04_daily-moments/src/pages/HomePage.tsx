bc9f4a90a82e68504e56f01eef6233e167e4553d
Michal Radomski <michalra@op.pl>
2024-05-31 22:34:03 +0200

    Ionic-React course - commit 29

--- 01 Ionic/04_daily-moments/src/pages/HomePage.tsx
+++ 01 Ionic/04_daily-moments/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
-import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem } from "@ionic/react";
+
+import { entries } from "../data";
 
 const HomePage: React.FC = () => {
   return (
@@ -11,6 +13,15 @@ const HomePage: React.FC = () => {
       <IonContent className="ion-padding">
         {/* Go to <IonRouterLink routerLink={"/settings"}>Settings</IonRouterLink> */}
         HomePage
+        <IonList>
+          {entries.map((entry, index) => {
+            return (
+              <IonItem key={entry.id + index} button={true} routerLink={`/entries/${entry.id}`}>
+                <IonLabel>{entry.title}</IonLabel>
+              </IonItem>
+            );
+          })}
+        </IonList>
       </IonContent>
     </IonPage>
   );
