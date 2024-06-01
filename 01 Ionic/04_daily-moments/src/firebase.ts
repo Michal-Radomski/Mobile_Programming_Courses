55a1c32d33f817a901e5c3815ef90d6fe0d334a2
Michal Radomski <michalra@op.pl>
2024-06-01 23:14:01 +0200

    Ionic-React course - commit 37

---
+++ 01 Ionic/04_daily-moments/src/firebase.ts
@@ -0,0 +1,12 @@
+import { getAuth } from "@firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "@firebase/storage";
+import { initializeApp } from "firebase/app";
+
+//* Firebase Config
+import { firebaseConfig } from "./firebaseConfig";
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
