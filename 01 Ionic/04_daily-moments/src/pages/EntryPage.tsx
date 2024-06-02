8a70a1d23210e7a197abe7329e3a22e34761e788
Michal Radomski <michalra@op.pl>
2024-06-03 01:45:07 +0200

    Ionic-React course - commit 47

--- 01 Ionic/04_daily-moments/src/pages/EntryPage.tsx
+++ 01 Ionic/04_daily-moments/src/pages/EntryPage.tsx
@@ -1,13 +1,24 @@
+import React from "react";
 import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from "@ionic/react";
 import { useParams } from "react-router-dom";
+import { doc, getDoc } from "@firebase/firestore";
 
-import { entries } from "../data";
+// import { entries } from "../data";
+import { Entry, RouteParams, toEntry } from "../Interfaces.d";
+import { firestore } from "../firebase";
 
 const EntryPage: React.FC = () => {
   const { id } = useParams<RouteParams>();
 
-  const entry = entries.find((entry) => entry.id === id);
+  // const entry = entries.find((entry) => entry.id === id);
   // console.log({ entry });
+
+  const [entry, setEntry] = React.useState<Entry>({} as Entry);
+
+  React.useEffect(() => {
+    const entryRef = doc(firestore, "entries", id);
+    getDoc(entryRef).then((doc) => setEntry(toEntry(doc)));
+  }, [id]);
 
   if (!entry) {
     throw new Error(`No such entry: ${id}`);
