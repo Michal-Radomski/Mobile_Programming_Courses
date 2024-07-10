e27cb2259ad875423790a908d1ba02a1d727273f
Michal Radomski <michalra@op.pl>
2024-07-11 00:44:04 +0200

    PWA course - commit 14

--- 02 PWA/02_PWAGram/src/src/ts/app.ts
+++ 02 PWA/02_PWAGram/src/src/ts/app.ts
@@ -47,8 +47,23 @@ if ("serviceWorker" in navigator) {
 // });
 
 //* Async JS
-setTimeout(() => {
-  console.log(1, "setTimeout");
-}, 3000);
+// setTimeout(() => {
+//   console.log(1, "setTimeout");
+// }, 3000);
 
-console.log(2, "After setTimeout");
+// console.log(2, "After setTimeout");
+
+const promise = new Promise<string>(function (_resolve, reject) {
+  setTimeout(() => {
+    // resolve("setTimeout");
+    reject("reject");
+  }, 3000);
+});
+
+// promise.then((text) => console.log(text));
+promise
+  .then((text: string) => {
+    return text;
+  })
+  .then((newText: string) => console.log({ newText }))
+  .catch((err: Error) => console.log({ err }));
