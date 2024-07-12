65cfb5c137fceddbdffc1c9cd0a975b706435f73
Michal Radomski <michalra@op.pl>
2024-07-12 23:20:08 +0200

    PWA course - commit 27

--- 02 PWA/02_PWAGram/src/sw.ts
+++ 02 PWA/02_PWAGram/src/sw.ts
@@ -1,10 +1,25 @@
 (self as any).addEventListener("install", function (event: CustomEvent) {
   // console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
-    caches.open("static").then(function (cache: Cache) {
+    caches.open("static").then(function (cache) {
       // console.log("cache:", cache);
       console.log("[Service Worker] Precaching App Shell");
-      cache.add("/src/js/app.js");
+      // cache.add("/src/js/app.js");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.min.js",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
     })
   );
 });
