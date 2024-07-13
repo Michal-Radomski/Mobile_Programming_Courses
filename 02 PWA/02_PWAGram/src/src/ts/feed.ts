5234803a0d57e8a9408a218af521108e31665b56
Michal Radomski <michalra@op.pl>
2024-07-13 19:17:20 +0200

    PWA course - commit 32

--- 02 PWA/02_PWAGram/src/src/ts/feed.ts
+++ 02 PWA/02_PWAGram/src/src/ts/feed.ts
@@ -30,6 +30,16 @@ shareImageButton.addEventListener("click", openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
 
+function onSaveButtonClicked(_event: Event) {
+  console.log("clicked");
+  // if ("caches" in window) {
+  //   caches.open("user-requested").then(function (cache) {
+  //     cache.add("https://httpbin.org/get");
+  //     cache.add("/src/images/sf-boat.jpg");
+  //   });
+  // }
+}
+
 function createCard(): void {
   const { componentHandler } = window;
   // console.log(componentHandler);
@@ -51,6 +61,12 @@ function createCard(): void {
   cardSupportingText.className = "mdl-card__supporting-text";
   cardSupportingText.textContent = "In San Francisco";
   cardSupportingText.style.textAlign = "center";
+
+  const cardSaveButton = document.createElement("button");
+  cardSaveButton.textContent = "Save";
+  cardSaveButton.addEventListener("click", onSaveButtonClicked);
+  cardSupportingText.appendChild(cardSaveButton);
+
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea?.appendChild(cardWrapper);
