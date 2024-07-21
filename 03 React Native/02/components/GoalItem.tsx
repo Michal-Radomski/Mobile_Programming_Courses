8e8fc2de76fcebb6ace0bdc8f5a3de6e1026ba5a
Michal Radomski <michalra@op.pl>
2024-07-22 01:25:20 +0200

    React Native course - commit 18

--- 03 React Native/02/components/GoalItem.tsx
+++ 03 React Native/02/components/GoalItem.tsx
@@ -5,10 +5,12 @@ const GoalItem = ({ text, id, onDeleteItem }: { text: string; id: string; onDele
     <Pressable
       // onPress={onDeleteItem.bind(this, id)}
       onPress={() => onDeleteItem(id)}
+      android_ripple={{ color: "#210644" }} //* Only Android!
+      style={({ pressed }: { pressed: boolean }) => pressed && styles.pressedItem} //* iOS
     >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>
-          {id} <Text style={{ color: "red" }}>{text}</Text>
+          {id} <Text style={{ color: "red", fontWeight: "bold" }}>{text}</Text>
         </Text>
       </View>
     </Pressable>
@@ -20,11 +22,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6, //* No borderRadius on iOS!
     backgroundColor: "#5e0acc",
   },
   goalText: {
+    padding: 8,
     color: "white", //* Styles don't cascade!!!, no style inheritance here
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
