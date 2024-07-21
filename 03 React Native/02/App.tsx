74b1b1d8effd1721d66f54d479c9611c7ac14f79
Michal Radomski <michalra@op.pl>
2024-07-21 12:33:17 +0200

    React Native course - commit 04

--- 03 React Native/02/App.tsx
+++ 03 React Native/02/App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={{ color: "red" }}>Hello World!</Text>
+      <View>
+        <Text>Another Text!</Text>
+      </View>
+
+      <Button title="Tap me" />
       <StatusBar style="auto" />
     </View>
   );
