337602508c99f08c200995626623263cccb7147a
Michal Radomski <michalra@op.pl>
2024-07-24 00:13:32 +0200

    React Native course - commit 25

--- 03 React Native/03/screens/StartGameScreen.tsx
+++ 03 React Native/03/screens/StartGameScreen.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = (): JSX.Element => {
   return (
     <React.Fragment>
-      <View>
-        <TextInput />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.numberInput} maxLength={2} />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
       </View>
@@ -16,3 +16,29 @@ const StartGameScreen = (): JSX.Element => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
