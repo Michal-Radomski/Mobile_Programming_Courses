8604b5339a6f14590955a8bc5cfc9e60e4c0f969
Michal Radomski <michalra@op.pl>
2024-07-26 00:20:34 +0200

    React Native course - commit 41

--- 03 React Native/03/screens/GameScreen.tsx
+++ 03 React Native/03/screens/GameScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/game/NumberContainer";
 import Title from "../components/ui/Title";
@@ -57,10 +58,14 @@ const GameScreen = ({ userNumber, onGameOver }: { userNumber: number; onGameOver
         <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+              <Ionicons name="remove" size={24} color="white" />
+            </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>+</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+              <Ionicons name="add" size={24} color="white" />
+            </PrimaryButton>
           </View>
         </View>
       </Card>
