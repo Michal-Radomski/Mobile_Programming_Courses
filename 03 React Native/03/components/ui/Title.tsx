b6e3494c4cda3127dea1b6bc6eda22aebc9637ce
Michal Radomski <michalra@op.pl>
2024-07-27 01:51:06 +0200

    React Native course - commit 48

--- 03 React Native/03/components/ui/Title.tsx
+++ 03 React Native/03/components/ui/Title.tsx
@@ -23,5 +23,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: Colors.colorWhite,
     padding: 12,
+    maxWidth: "80%",
+    width: 300,
   },
 });
--- 03 React Native/03/screens/GameOverScreen.tsx
+++ 03 React Native/03/screens/GameOverScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Image, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet, Dimensions } from "react-native";
 
 import Title from "../components/ui/Title";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -30,6 +30,8 @@ function GameOverScreen({
 
 export default GameOverScreen;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
@@ -38,9 +40,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: deviceWidth < 380 ? 150 : 300,
+    height: deviceWidth < 380 ? 150 : 300,
+    borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: "hidden",
