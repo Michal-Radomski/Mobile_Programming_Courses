78a85eadc30e94e5c37eac9e8867c8fee3f3735a
Michal Radomski <michalra@op.pl>
2024-07-29 00:41:53 +0200

    React Native course - commit 63

--- 03 React Native/04_Meal_App/screens/MealsOverviewScreen.tsx
+++ 03 React Native/04_Meal_App/screens/MealsOverviewScreen.tsx
@@ -1,13 +1,34 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { useRoute, RouteProp, ParamListBase } from "@react-navigation/native";
+
+import { MEALS } from "../data/dummy-data";
+import Meal from "../models/meal";
+import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen(): JSX.Element {
   const route: RouteProp<ParamListBase> = useRoute();
   const catId = (route?.params as any)?.categoryId as string;
 
+  const displayedMeals: Meal[] = MEALS.filter((mealItem: Meal) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData: { item: Meal }): JSX.Element {
+    const item: Meal = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item: Meal) => item.id} renderItem={renderMealItem} />
     </View>
   );
 }
