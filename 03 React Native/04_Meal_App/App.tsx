58bee350ee83fa00dca79a01f831c6b43588a8f1
Michal Radomski <michalra@op.pl>
2024-07-28 19:31:44 +0200

    React Native course - commit 59

--- 03 React Native/04_Meal_App/App.tsx
+++ 03 React Native/04_Meal_App/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CategoriesScreen from "./screens/CategoriesScreens";
+
+const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
   return (
     <React.Fragment>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </React.Fragment>
   );
 }
