5ffdbbb1130eec81a3d447f6c67cb1a39bdb8553
Michal Radomski <michalra@op.pl>
2024-07-28 18:55:45 +0200

    React Native course - commit 57

--- 03 React Native/04_Meal_App/App.tsx
+++ 03 React Native/04_Meal_App/App.tsx
@@ -1,20 +1,18 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+import CategoriesScreen from "./screens/CategoriesScreens";
 
 export default function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.Fragment>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: {},
 });
---
+++ 03 React Native/04_Meal_App/screens/CategoriesScreens.tsx
@@ -0,0 +1,17 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+import { CATEGORIES } from "../data/dummy-data";
+import Category from "../models/category";
+
+function renderCategoryItem(itemData: { item: Category }): JSX.Element {
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
+}
+
+function CategoriesScreen(): JSX.Element {
+  return (
+    <FlatList data={CATEGORIES} keyExtractor={(item: Category) => item.id} renderItem={renderCategoryItem} numColumns={2} />
+  );
+}
+
+export default CategoriesScreen;
