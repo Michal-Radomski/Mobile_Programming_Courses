7e96285655375d6dc3adf6128fac13ce7f896394
Michal Radomski <michalra@op.pl>
2024-07-30 00:16:02 +0200

    React Native course - commit 74

--- 03 React Native/05/screens/UserScreen.tsx
+++ 03 React Native/05/screens/UserScreen.tsx
@@ -1,17 +1,12 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import { ParamListBase, NavigationProp } from "@react-navigation/native";
+//* Bottom Tabs
+import { View, Text, StyleSheet } from "react-native";
 
-function UserScreen({ navigation }: { navigation: NavigationProp<ParamListBase> }): JSX.Element {
-  function openDrawerHandler(): void {
-    (navigation as any).toggleDrawer();
-  }
-
+function UserScreen(): JSX.Element {
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
-      <Button title="Open Drawer" onPress={openDrawerHandler} />
     </View>
   );
 }
@@ -29,3 +24,36 @@ const styles = StyleSheet.create({
     color: "#eb1064",
   },
 });
+
+//* Drawer
+// import { View, Text, Button, StyleSheet } from "react-native";
+// import { ParamListBase, NavigationProp } from "@react-navigation/native";
+
+// function UserScreen({ navigation }: { navigation: NavigationProp<ParamListBase> }): JSX.Element {
+//   function openDrawerHandler(): void {
+//     (navigation as any).toggleDrawer();
+//   }
+
+//   return (
+//     <View style={styles.rootContainer}>
+//       <Text>
+//         This is the <Text style={styles.highlight}>"User"</Text> screen!
+//       </Text>
+//       <Button title="Open Drawer" onPress={openDrawerHandler} />
+//     </View>
+//   );
+// }
+
+// export default UserScreen;
+
+// const styles = StyleSheet.create({
+//   rootContainer: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   highlight: {
+//     fontWeight: "bold",
+//     color: "#eb1064",
+//   },
+// });
