df4da4c2bc70de044264c96e02fd8c0666482b8e
Michal Radomski <michalra@op.pl>
2024-07-31 00:57:36 +0200

    React Native course - commit 89

--- 03 React Native/06_Expense_Tracker/components/ExpensesList.tsx
+++ 03 React Native/06_Expense_Tracker/components/ExpensesList.tsx
@@ -1,10 +1,12 @@
-import { FlatList, Text } from "react-native";
+import { FlatList } from "react-native";
 
 import { ObjectI } from "../App";
+import ExpenseItem from "./ExpenseItem";
 
 function renderExpenseItem(itemData: { item: ObjectI }): JSX.Element {
   // console.log("itemData:", itemData);
-  return <Text>{itemData.item.description as string}</Text>;
+  // return <Text>{itemData.item.description as string}</Text>;
+  return <ExpenseItem {...(itemData.item as ObjectI)} />;
 }
 
 function ExpensesList({ expenses }: { expenses: ObjectI[] }): JSX.Element {
