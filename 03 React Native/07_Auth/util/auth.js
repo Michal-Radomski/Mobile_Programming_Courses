6c387233c8cb55d1a65fc34498dc3dacd51078cb
Michal Radomski <michalra@op.pl>
2024-08-06 00:14:49 +0200

    React Native course - commit 120

---
+++ 03 React Native/07_Auth/util/auth.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+//* Not hosted on Github!
+const API_KEY = "AIzaSyCiMjLJ2MXFXQDwRHfWyI0_F0v3CvjISck";
+
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  //* Warning axios 404?
+  try {
+    const response = await axios.post(url, {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    });
+
+    const token = response.data.idToken;
+    // console.log({ token });
+
+    return token;
+  } catch (error) {
+    console.log("error:", error);
+  }
+}
+
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
+}
+
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
+}
